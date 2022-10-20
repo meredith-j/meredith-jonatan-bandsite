5346cd6d61220ddcc082a5e3b09ef4bcd9b4b08a
Meredith Jonatan <mjonatan@me.com>
2022-10-19 23:45:53 -0400

    fix class names

---
+++ scripts/index-page.js
@@ -0,0 +1,40 @@
+/* 
+here is user comment code structure without assigned values, just in case i need
+it again.
+
+    {
+    id: #,
+    userName: "",
+    commentDate: "",
+    commentContent: "",
+}; */
+
+
+
+let comments = [
+    {
+        id: 1,
+        userName: "Connor Walton",
+        commentDate: "02/17/2021",
+        commentContent: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+    },
+    {
+        id: 2,
+        userName: "Emilie Beach",
+        commentDate: "01/09/2021",
+        commentContent: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+    },
+    {
+        id: 3,
+        userName: "Miles Acosta",
+        commentDate: "12/20/2020",
+        commentContent: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+    },    
+];
+
+
+/*
+userComment() (
+    console.log(`this comment is`)
+)
+*/
